50b009c5723f3ef903445fc16e2eea9742832ca1
kkhhjjoo <khjoo1203@gmail.com>
2026-02-06 11:40:00 +0900

    프론트엔드 crud함

--- src/App.js
+++ src/App.js
@@ -11,10 +11,55 @@ import api from './utils/api';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState('');
 
   const getTasks = async () => {
     const response = await api.get('/tasks');
     console.log('response', response);
+    setTodoList(response.data.data);
+  }
+  const addTask = async () => {
+    try {
+      const response = await api.post('/tasks', {
+        task: todoValue,
+        isComplete: false
+      });
+      if (response.status === 200) {
+        console.log('성공');
+        //1. 입력한 값이 안사라짐
+        setTodoValue("");
+        //2. 추가한 값이 안보임
+        getTasks();
+      } else { 
+        throw new Error('할일이 추가할수 없습니다')
+      }
+    } catch (err) { 
+      console.log('error', err);
+    }
+  }
+  const toggleComplete = async (id) => { 
+    try {
+      const task = todoList.find((item) => item._id === id);
+      const response = await api.put(`/tasks/${id}`, {
+        isComplete: !task.isComplete
+      });
+      if (response.status === 200) { 
+        getTasks();
+      }
+    } catch (error) { 
+      console.log('error', error);
+    }
+  }
+  const deleteItem = async (id) => {
+    try {
+      console.log(id);
+      const response = await api.delete(`/tasks/${id}`);
+      if (response.status === 200) {
+        getTasks();
+      }
+    } catch (error) { 
+      console.log('error', error);
+    }
   }
   useEffect(() => { 
     getTasks()
@@ -27,14 +72,16 @@ function App() {
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>추가</button>
         </Col>
       </Row>
 
-      <TodoBoard />
+      <TodoBoard todoList={todoList} toggleComplete={toggleComplete} deleteItem={deleteItem} />
     </Container>
   );
 }
