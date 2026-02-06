75db33080481344d748257ec1a4916f294bc3951
kkhhjjoo <khjoo1203@gmail.com>
2026-02-06 18:27:54 +0900

    if뒤에 줄 수정

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import TodoBoard from "./components/TodoBoard";
-
+import api from "./utils/api";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import api from './utils/api';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [todoValue, setTodoValue] = useState('');
+  const [todoValue, setTodoValue] = useState("");
 
   const getTasks = async () => {
-    const response = await api.get('/tasks');
-    console.log('response', response);
+    const response = await api.get("/tasks");
     setTodoList(response.data.data);
-  }
-  const addTask = async () => {
+  };
+  useEffect(() => {
+    getTasks();
+  }, []);
+  const addTodo = async () => {
     try {
-      const response = await api.post('/tasks', {
+      const response = await api.post("/tasks", {
         task: todoValue,
-        isComplete: false
+        isComplete: false,
       });
       if (response.status === 200) {
-        console.log('성공');
-        //1. 입력한 값이 안사라짐
-        setTodoValue("");
-        //2. 추가한 값이 안보임
-        getTasks();
-      } else { 
-        throw new Error('할일이 추가할수 없습니다')
-      }
-    } catch (err) { 
-      console.log('error', err);
-    }
-  }
-  const toggleComplete = async (id) => { 
-    try {
-      const task = todoList.find((item) => item._id === id);
-      const response = await api.put(`/tasks/${id}`, {
-        isComplete: !task.isComplete
-      });
-      if (response.status === 200) { 
         getTasks();
       }
-    } catch (error) { 
-      console.log('error', error);
+      setTodoValue("");
+    } catch (error) {
+      console.log("error:", error);
     }
-  }
+  };
+
   const deleteItem = async (id) => {
     try {
       console.log(id);
@@ -57,13 +41,24 @@ function App() {
       if (response.status === 200) {
         getTasks();
       }
-    } catch (error) { 
-      console.log('error', error);
+    } catch (error) {
+      console.log("error", error);
     }
-  }
-  useEffect(() => { 
-    getTasks()
-  }, []);
+  };
+
+  const toggleComplete = async (id) => {
+    try {
+      const task = todoList.find((item) => item._id === id);
+      const response = await api.put(`/tasks/${id}`, {
+        isComplete: !task.isComplete,
+      });
+      if (response.status === 200) {
+        getTasks();
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <Container>
       <Row className="add-item-row">
@@ -71,17 +66,23 @@ function App() {
           <input
             type="text"
             placeholder="할일을 입력하세요"
+            onChange={(event) => setTodoValue(event.target.value)}
             className="input-box"
             value={todoValue}
-            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>추가</button>
+          <button onClick={addTodo} className="button-add">
+            추가
+          </button>
         </Col>
       </Row>
 
-      <TodoBoard todoList={todoList} toggleComplete={toggleComplete} deleteItem={deleteItem} />
+      <TodoBoard
+        todoList={todoList}
+        deleteItem={deleteItem}
+        toggleComplete={toggleComplete}
+      />
     </Container>
   );
 }
